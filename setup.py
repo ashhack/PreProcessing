c1ac5aad262071b4baca8f5725307fe2f597878e
ashhack <ashutosh71195@gmail.com>
2018-08-04 15:06:51 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PreProcessing",
+    version="0.0.1",
+    author="Ashutosh Singh",
+    author_email="ashutosh71195@gmail.com",
+    description="Package for preprocessing image, text and tabular data for ML, DL algorithms",
+    long_description=long_description,
+    long_description_content_type="markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
+
